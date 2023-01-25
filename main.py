71a46728877c6badbe37ea99ee2416c781912a05
mchovan <viktorijagrigojeva@gmail.com>
2023-01-25 15:37:26 +0100

    added if name == main + cleaned redundant staff + added icon of sky condition when running next_12h_simplified method

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import requests
-# import pprint
 
 api_key = "692c67f68c0d645297530d27416d3e02"
 his_api = "26631f0f41b95fb9f5ac0df9a8f43c92"
-
-# print(jsa['list'][0]['main']['temp'])
 
 
 class Weather:
@@ -23,32 +20,27 @@ class Weather:
         else:
             raise AttributeError("provide either city or lon and lat as arguments")
 
+        if self.data['cod'] != "200":
+            raise ValueError(f"{self.data['message']}")
+
     def next_12h(self):
         return self.data['list'][:4]
 
     def next_12h_simplified(self):
-        try:
-            interesting = self.data['list'][:4]
+        interesting = self.data['list'][:4]
 
-            result = ""
-            for x in interesting:
-                descript = x['weather'][0]['description']
-                date_t = x['dt_txt'].split(' ')[0]
-                time_t = x['dt_txt'].split(' ')[1]
-                value = x['main']['temp']
-                result += (f"On {date_t} at {time_t} will be {value} celsius description: {descript} \n")
+        result = ""
+        for x in interesting:
+            descript = x['weather'][0]['description']
+            icon = x['weather'][0]['icon']
+            date_t = x['dt_txt'].split(' ')[0]
+            time_t = x['dt_txt'].split(' ')[1]
+            value = x['main']['temp']
+            result += (f"On {date_t} at {time_t} will be {value} celsius, description - {descript}, icon - {icon} \n")
 
-            return result
+        return result
 
-        except KeyError:
-            return f"Error {self.data['cod']}, {self.data['message']}"
 
-# self.data['list'][0]['main']['temp']
-# wet = Weather(location='Dubaj')
-# print(wet.data)
-
-sec_wet = Weather(city='Madriddd')
-
-# pprint.pprint(sec_wet.next_12h())
-# print(sec_wet.next_12h_simplified())
-print(sec_wet.next_12h_simplified())
+if __name__ == '__main__':
+    sec_wet = Weather(city='Madrid')
+    print(sec_wet.next_12h_simplified())
